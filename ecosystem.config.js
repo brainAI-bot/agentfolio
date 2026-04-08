c9c17c7459a6e63edbd4025d5d545e541ddeb24e
Ubuntu <ubuntu@ip-172-31-5-243.eu-north-1.compute.internal>
2026-04-08 19:30:34 +0000

    Use funded signer for SATP mainnet writes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       BOA_CLUSTER: "mainnet",
       SATP_NETWORK: "mainnet",
       SOLANA_RPC_URL: "https://mainnet.helius-rpc.com/?api-key=91c63e44-1c7a-4b98-830b-6135632565fb",
-      SATP_PLATFORM_KEYPAIR: "/home/ubuntu/.config/solana/satp-mainnet-platform.json"
+      SATP_PLATFORM_KEYPAIR: "/home/ubuntu/.config/solana/mainnet-deployer.json"
     },
     kill_timeout: 5000,
     listen_timeout: 8000,
