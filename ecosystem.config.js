d5a309bfb863b9403ed3c92a2be7305d75e7a876
brainForge <brainforge@agentfolio.bot>
2026-04-11 21:13:26 +0000

    Switch x402 facilitator to PayAI

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,13 @@ module.exports = {
       BOA_CLUSTER: "mainnet",
       SATP_NETWORK: "mainnet",
       SOLANA_RPC_URL: "https://mainnet.helius-rpc.com/?api-key=91c63e44-1c7a-4b98-830b-6135632565fb",
-      SATP_PLATFORM_KEYPAIR: "/home/ubuntu/.config/solana/mainnet-deployer.json"
+      SATP_PLATFORM_KEYPAIR: "/home/ubuntu/.config/solana/mainnet-deployer.json",
+      X402_ENABLED: "true",
+      X402_SCHEME: "svm",
+      X402_NETWORK: "solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp",
+      X402_FACILITATOR: "https://facilitator.payai.network",
+      X402_RECEIVE_ADDRESS: "FriU1FEpWbdgVrTcS49YV5mVv2oqN6poaVQjzq2BS5be",
+      X402_RECEIVING_ADDRESS: "FriU1FEpWbdgVrTcS49YV5mVv2oqN6poaVQjzq2BS5be"
     },
     kill_timeout: 5000,
     listen_timeout: 8000,
