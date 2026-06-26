41b857c55ad23b17fdea81e2bbfe405689dec0c3
brainForge <brainforge@brainai.bot>
2026-06-26 11:50:20 +0300

    Complete SATP client boundary cleanup

--- src/lib/satp-verification-bridge.js
+++ src/lib/satp-verification-bridge.js
@@ -13,7 +13,7 @@
 const { Connection, Keypair, PublicKey, SystemProgram, Transaction, TransactionInstruction, ComputeBudgetProgram } = require('@solana/web3.js');
 const crypto = require('crypto');
 const fs = require('fs');
-const { getV3AttestationPDA, getGenesisPDA: getCanonicalGenesisPDA, getV3ValidationAuthorityPDA, getV3ProgramIds } = require('../satp-client/src/v3-pda');
+const { getV3AttestationPDA, getGenesisPDA: getCanonicalGenesisPDA, getV3ValidationAuthorityPDA, getV3ProgramIds } = require('@brainai/satp-client');
 
 // V3 Program IDs (mainnet)
 const ATTESTATIONS_PROGRAM = new PublicKey('6Xd1dAQJPvQRJ4Ntr6LtPTjDjPUZ8nfnmYLZaZ2DtrdD');
@@ -464,4 +464,3 @@ module.exports = {
   getAgentAttestations,
   triggerRecomputeOnly,
 };
-
